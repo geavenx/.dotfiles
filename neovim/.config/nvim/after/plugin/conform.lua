db38f1470b45a71c64b8e86222c90cb71d2c2be1
geavenx <vitordonnangeloc@gmail.com>
2024-01-07 17:10:41 -0300

    [+] sql formmatting with sqlfmt

--- neovim/.config/nvim/after/plugin/conform.lua
+++ neovim/.config/nvim/after/plugin/conform.lua
@@ -1,31 +1,32 @@
 require("conform").setup({
 
-    formatters_by_ft = {
-        lua = { "stylua" },
-        python = { "black" },
-        javascript = { "prettierd" },
-        typescript = { "prettierd" },
-        typescriptreact = { "prettierd" },
-        javascriptreact = { "prettierd" },
-        html = { "prettierd" },
-        css = { "prettierd" },
-        json = { "prettierd" },
-        yaml = { "prettierd" },
-        toml = { "prettierd" },
-        rust = { "rustfmt" },
-        go = { "gofmt" },
-    },
-    format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-    },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "black" },
+		javascript = { "prettierd" },
+		typescript = { "prettierd" },
+		typescriptreact = { "prettierd" },
+		javascriptreact = { "prettierd" },
+		html = { "prettierd" },
+		css = { "prettierd" },
+		json = { "prettierd" },
+		yaml = { "prettierd" },
+		toml = { "prettierd" },
+		rust = { "rustfmt" },
+		go = { "gofmt" },
+		sql = { "sqlfmt" },
+	},
+	format_on_save = {
+		lsp_fallback = true,
+		async = false,
+		timeout_ms = 1000,
+	},
 
-    vim.keymap.set("n", "<leader>f", function()
-        require("conform").format({
-            lsp_fallback = true,
-            async = false,
-            timeout_ms = 1000,
-        })
-    end, { desc = "<f>ormat file" }),
+	vim.keymap.set("n", "<leader>f", function()
+		require("conform").format({
+			lsp_fallback = true,
+			async = false,
+			timeout_ms = 1000,
+		})
+	end, { desc = "<f>ormat file" }),
 })
