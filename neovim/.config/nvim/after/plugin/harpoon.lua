993342da2c58e80e5ab021c04151c4e28fa64567
geavenx <vitordonnangeloc@gmail.com>
2024-01-07 22:16:28 -0300

    Changed mappings

--- neovim/.config/nvim/after/plugin/harpoon.lua
+++ neovim/.config/nvim/after/plugin/harpoon.lua
@@ -1,9 +1,8 @@
-local harpoon = require('harpoon')
+local harpoon = require("harpoon")
 
 harpoon:setup({})
 
-local conf = require('telescope.config').values
-
+local conf = require("telescope.config").values
 
 local function toggle_telescope(harpoon_files)
 	local file_paths = {}
@@ -11,19 +10,34 @@ local function toggle_telescope(harpoon_files)
 		table.insert(file_paths, item.value)
 	end
 
-	require('telescope.pickers').new({}, {
-		prompt_title = 'Harpoon',
-		finder = require('telescope.finders').new_table({
-			results = file_paths,
-		}),
-		previewer = conf.file_previewer({}),
-		sorter = conf.generic_sorter({}),
-	}):find()
+	require("telescope.pickers")
+		.new({}, {
+			prompt_title = "Harpoon",
+			finder = require("telescope.finders").new_table({
+				results = file_paths,
+			}),
+			previewer = conf.file_previewer({}),
+			sorter = conf.generic_sorter({}),
+		})
+		:find()
 end
 
-vim.keymap.set('n', '<C-e>', function() toggle_telescope(harpoon:list()) end, { desc = 'Open Harpoon window' })
-vim.keymap.set('n', '<leader>ha', function() harpoon:list():append() end, { desc = '<h>arpoon <a>dd buffer to list' })
-vim.keymap.set('n', '<C-1>', function() harpoon:list():select(1) end, { desc = 'Go to item 1 of the list' })
-vim.keymap.set('n', '<C-2>', function() harpoon:list():select(2) end, { desc = 'Go to item 2 of the list' })
-vim.keymap.set('n', '<C-3>', function() harpoon:list():select(3) end, { desc = 'Go to item 3 of the list' })
-vim.keymap.set('n', '<C-4>', function() harpoon:list():select(4) end, { desc = 'Go to item 4 of the list' })
+vim.keymap.set("n", "<C-e>", function()
+	toggle_telescope(harpoon:list())
+end, { desc = "Open Harpoon window" })
+vim.keymap.set("n", "<leader>ha", function()
+	harpoon:list():append()
+end, { desc = "<h>arpoon <a>dd buffer to list" })
+
+vim.keymap.set("n", "<leader>1", function()
+	harpoon:list():select(1)
+end, { desc = "Go to item 1 of the list" })
+vim.keymap.set("n", "<leader>2", function()
+	harpoon:list():select(2)
+end, { desc = "Go to item 2 of the list" })
+vim.keymap.set("n", "<leader>3", function()
+	harpoon:list():select(3)
+end, { desc = "Go to item 3 of the list" })
+vim.keymap.set("n", "<leader>4", function()
+	harpoon:list():select(4)
+end, { desc = "Go to item 4 of the list" })
