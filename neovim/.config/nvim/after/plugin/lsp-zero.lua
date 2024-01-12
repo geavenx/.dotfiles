ba16738a35b2fb7fed0b5c92b9067cb68309edd4
geavenx <vitordonnangeloc@gmail.com>
2024-01-12 20:29:50 -0300

    fix: completions

--- neovim/.config/nvim/after/plugin/lsp-zero.lua
+++ neovim/.config/nvim/after/plugin/lsp-zero.lua
@@ -1,6 +1,34 @@
 -- Better Lua LSP
 require("neodev").setup()
 
+local cmp = require("cmp")
+--local cmp_action = require("lsp-zero").cmp_action()
+
+require("luasnip.loaders.from_vscode").lazy_load()
+
+cmp.setup({
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+	},
+
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+
+	mapping = cmp.mapping.preset.insert({
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
+		["<C-Space>"] = cmp.mapping.complete(),
+	}),
+
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
+})
+
 local lsp_zero = require("lsp-zero") --.preset({})
 
 lsp_zero.on_attach(function(client, bufnr)
@@ -26,34 +54,7 @@ require("mason-lspconfig").setup({
 		"lua_ls",
 		"tsserver",
 	},
-	handlers = { lsp_zero.default_setup },
-})
-
-lsp_zero.setup_servers({ "pyright", "gopls", "lua_ls", "tsserver", "html-lsp" })
-
-local cmp = require("cmp")
-local cmp_action = require("lsp-zero").cmp_action()
-
-require("luasnip.loaders.from_vscode").lazy_load()
-
-cmp.setup({
-	sources = {
-		{ name = "luasnip" },
-	},
-
-	window = {
-		completion = cmp.config.window.bordered(),
-		documentation = cmp.config.window.bordered(),
-	},
-
-	mapping = cmp.mapping.preset.insert({
-		["<CR>"] = cmp.mapping.confirm({ select = false }),
-		["<C-Space>"] = cmp.mapping.complete(),
-	}),
-
-	snippet = {
-		expand = function(args)
-			require("luasnip").lsp_expand(args.body)
-		end,
+	handlers = {
+		lsp_zero.default_setup,
 	},
 })
