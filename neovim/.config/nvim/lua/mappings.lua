4cecebb7d3d7ee0528be809e8cb4065b0178d931
geavenx <vitordonnangeloc@gmail.com>
2024-01-16 17:34:12 -0300

    feat: visual mode J and K mappings added

--- neovim/.config/nvim/lua/mappings.lua
+++ neovim/.config/nvim/lua/mappings.lua
@@ -4,10 +4,10 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "<p>roject <v>iew" })
 vim.keymap.set({ "v", "n" }, "<leader>y", '"+y', { desc = "yank to clipboard" })
 
 vim.keymap.set(
-    "n",
-    "<leader>s",
-    [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
-    { desc = "<s>earch and replace all" }
+	"n",
+	"<leader>s",
+	[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+	{ desc = "<s>earch and replace all" }
 )
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "scroll down keeping the cursor on the center" })
@@ -20,3 +20,6 @@ vim.keymap.set("n", "<C-t>", "<cmd>:TransparentToggle<CR>", { desc = "toggle tra
 vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "<p>aste overwriting * not yanking" })
 
 vim.keymap.set("n", "<leader>gd", "<cmd>:DiffviewOpen<CR>", { desc = "<g>it <d>iff view open" })
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
