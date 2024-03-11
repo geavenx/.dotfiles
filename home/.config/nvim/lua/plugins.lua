7db923a4b5e573ec53d38463375b91ce88ed88e6
vitor <vitordonnangeloc@proton.me>
2024-03-11 19:48:29 -0300

    feat: add .yuck (eww) language support

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -1,184 +1,186 @@
 return {
 
-    -------------------------------------------------------
-    -- Buffer management
-    -------------------------------------------------------
+	-------------------------------------------------------
+	-- Buffer management
+	-------------------------------------------------------
 
-    -- Harpoon2 (https://github.com/ThePrimeagen/harpoon/tree/harpoon2)
-    --
-    {
-        "ThePrimeagen/harpoon",
-        branch = "harpoon2",
-        dependencies = { "nvim-telescope/telescope.nvim" },
-    },
+	-- Harpoon2 (https://github.com/ThePrimeagen/harpoon/tree/harpoon2)
+	--
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-telescope/telescope.nvim" },
+	},
 
-    -- Telescope (https://github.com/nvim-telescope/telescope.nvim)
-    --
-    {
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.5",
-        dependencies = { "nvim-lua/plenary.nvim" },
-    },
+	-- Telescope (https://github.com/nvim-telescope/telescope.nvim)
+	--
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
 
-    -- Treesitter (https://github.com/nvim-treesitter/nvim-treesitter)
-    --
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-    },
+	-- Treesitter (https://github.com/nvim-treesitter/nvim-treesitter)
+	--
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+	},
 
-    -- FTerm (https://github.com/numToStr/FTerm.nvim)
-    --
+	-- FTerm (https://github.com/numToStr/FTerm.nvim)
+	--
 
-    { "numToStr/FTerm.nvim" },
+	{ "numToStr/FTerm.nvim" },
 
-    -------------------------------------------------------
-    -- UI & UX Improvements
-    -------------------------------------------------------
+	-------------------------------------------------------
+	-- UI & UX Improvements
+	-------------------------------------------------------
 
-    -- Which-key (https://github.com/folke/which-key.nvim)
-    --
-    {
-        "folke/which-key.nvim",
-        event = "VeryLazy",
-        init = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 300
-        end,
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-        },
-    },
+	-- Which-key (https://github.com/folke/which-key.nvim)
+	--
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+		},
+	},
 
-    -- Undotree (https://github.com/mbbill/undotree)
-    --
-    { "mbbill/undotree" },
+	-- Undotree (https://github.com/mbbill/undotree)
+	--
+	{ "mbbill/undotree" },
 
-    -- Transparent (https://github.com/xiyaowong/transparent.nvim)
-    --
-    { "xiyaowong/transparent.nvim" },
+	-- Transparent (https://github.com/xiyaowong/transparent.nvim)
+	--
+	{ "xiyaowong/transparent.nvim" },
 
-    -- nvim-web-devicons (https://github.com/nvim-tree/nvim-web-devicons)
-    --
-    { "nvim-tree/nvim-web-devicons" },
+	-- nvim-web-devicons (https://github.com/nvim-tree/nvim-web-devicons)
+	--
+	{ "nvim-tree/nvim-web-devicons" },
 
-    -------------------------------------------------------
-    -- LSP and Formatting
-    -------------------------------------------------------
+	-------------------------------------------------------
+	-- LSP and Formatting
+	-------------------------------------------------------
 
-    -- Mason arsenal!
-    -- (https://github.com/williamboman/mason.nvim) && /
-    -- (https://github.com/williamboman/mason-lspconfig.nvim) && /
-    -- (https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim)
-    --
-    { "williamboman/mason.nvim" },
-    { "williamboman/mason-lspconfig.nvim" },
-    { "WhoIsSethDaniel/mason-tool-installer.nvim" },
+	-- Mason arsenal!
+	-- (https://github.com/williamboman/mason.nvim) && /
+	-- (https://github.com/williamboman/mason-lspconfig.nvim) && /
+	-- (https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim)
+	--
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
+	{ "WhoIsSethDaniel/mason-tool-installer.nvim" },
 
-    -- Lsp-zero (https://github.com/VonHeikemen/lsp-zero.nvim/tree/v3.x)
-    --
-    { "VonHeikemen/lsp-zero.nvim",                branch = "v3.x" },
+	-- Lsp-zero (https://github.com/VonHeikemen/lsp-zero.nvim/tree/v3.x)
+	--
+	{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
 
-    -- Lspconfig (https://github.com/neovim/nvim-lspconfig)
-    --
-    { "neovim/nvim-lspconfig" },
+	-- Lspconfig (https://github.com/neovim/nvim-lspconfig)
+	--
+	{ "neovim/nvim-lspconfig" },
 
-    -- cmp-nvim-lsp (https://github.com/hrsh7th/cmp-nvim-lsp)
-    --
-    { "hrsh7th/cmp-nvim-lsp" },
+	-- cmp-nvim-lsp (https://github.com/hrsh7th/cmp-nvim-lsp)
+	--
+	{ "hrsh7th/cmp-nvim-lsp" },
 
-    -- nvim-cmp (https://github.com/hrsh7th/nvim-cmp)
-    --
-    { "hrsh7th/nvim-cmp" },
+	-- nvim-cmp (https://github.com/hrsh7th/nvim-cmp)
+	--
+	{ "hrsh7th/nvim-cmp" },
 
-    -- Lua-snip (https://github.com/L3MON4D3/LuaSnip)
-    --
-    {
-        "L3MON4D3/LuaSnip",
-        dependencies = { "rafamadriz/friendly-snippets" },
-        version = "v2.*",
-        build = "make install_jsregexp",
-    },
+	-- Lua-snip (https://github.com/L3MON4D3/LuaSnip)
+	--
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = { "rafamadriz/friendly-snippets" },
+		version = "v2.*",
+		build = "make install_jsregexp",
+	},
 
-    -- cmp-luasnip (https://github.com/saadparwaiz1/cmp_luasnip)
-    --
-    { "saadparwaiz1/cmp_luasnip" },
+	-- cmp-luasnip (https://github.com/saadparwaiz1/cmp_luasnip)
+	--
+	{ "saadparwaiz1/cmp_luasnip" },
 
-    -- NeoDev (https://github.com/folke/neodev.nvim)
-    --
-    { "folke/neodev.nvim",                        opts = {} },
+	-- NeoDev (https://github.com/folke/neodev.nvim)
+	--
+	{ "folke/neodev.nvim", opts = {} },
 
-    -- conform (https://github.com/stevearc/conform.nvim)
-    --
-    { "stevearc/conform.nvim",                    opts = {} },
+	-- conform (https://github.com/stevearc/conform.nvim)
+	--
+	{ "stevearc/conform.nvim", opts = {} },
 
-    -------------------------------------------------------
-    -- Git helpers
-    -------------------------------------------------------
+	{ "elkowar/yuck.vim" },
 
-    -- Diffview (https://github.com/sindrets/diffview.nvim)
-    --
-    { "https://github.com/sindrets/diffview.nvim" },
+	-------------------------------------------------------
+	-- Git helpers
+	-------------------------------------------------------
 
-    -- vim-fugitive (https://github.com/tpope/vim-fugitive)
-    --
-    { "tpope/vim-fugitive" },
+	-- Diffview (https://github.com/sindrets/diffview.nvim)
+	--
+	{ "https://github.com/sindrets/diffview.nvim" },
 
-    -------------------------------------------------------
-    -- AI Coding
-    -------------------------------------------------------
+	-- vim-fugitive (https://github.com/tpope/vim-fugitive)
+	--
+	{ "tpope/vim-fugitive" },
 
-    -- Github Copilot (https://github.com/github/copilot.vim)
-    --
-    { "github/copilot.vim" },
+	-------------------------------------------------------
+	-- AI Coding
+	-------------------------------------------------------
 
-    -------------------------------------------------------
-    -- Editing
-    -------------------------------------------------------
+	-- Github Copilot (https://github.com/github/copilot.vim)
+	--
+	{ "github/copilot.vim" },
 
-    -- Autoclose (https://github.com/m4xshen/autoclose.nvim)
-    --
-    { "m4xshen/autoclose.nvim" },
+	-------------------------------------------------------
+	-- Editing
+	-------------------------------------------------------
 
-    -------------------------------------------------------
-    -- colorschemes
-    -------------------------------------------------------
+	-- Autoclose (https://github.com/m4xshen/autoclose.nvim)
+	--
+	{ "m4xshen/autoclose.nvim" },
 
-    -- Kanagawa (https://github.com/rebelot/kanagawa.nvim)
-    --
-    { "rebelot/kanagawa.nvim" },
+	-------------------------------------------------------
+	-- colorschemes
+	-------------------------------------------------------
 
-    -- Tokyonight (https://github.com/folke/tokyonight.nvim)
-    --
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {},
-    },
+	-- Kanagawa (https://github.com/rebelot/kanagawa.nvim)
+	--
+	{ "rebelot/kanagawa.nvim" },
 
-    -------------------------------------------------------
-    -- Debugging
-    -------------------------------------------------------
+	-- Tokyonight (https://github.com/folke/tokyonight.nvim)
+	--
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
 
-    -- Nvim-dap (https://github.com/mfussenegger/nvim-dap)
-    --
-    { "mfussenegger/nvim-dap" },
+	-------------------------------------------------------
+	-- Debugging
+	-------------------------------------------------------
 
-    -- Mason-nvim-dap (https://github.com/jay-babu/mason-nvim-dap.nvim)
-    --
-    {
-        "jay-babu/mason-nvim-dap.nvim",
-        dependencies = { "williamboman/mason.nvim", "mfussenegger/nvim-dap" },
-        event = "VeryLazy",
-    },
+	-- Nvim-dap (https://github.com/mfussenegger/nvim-dap)
+	--
+	{ "mfussenegger/nvim-dap" },
 
-    -- Nvim-dap-ui (https://github.com/rcarriga/nvim-dap-ui)
-    --
-    {
-        "rcarriga/nvim-dap-ui",
-        dependencies = { "mfussenegger/nvim-dap" },
-        event = "VeryLazy",
-    },
+	-- Mason-nvim-dap (https://github.com/jay-babu/mason-nvim-dap.nvim)
+	--
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		dependencies = { "williamboman/mason.nvim", "mfussenegger/nvim-dap" },
+		event = "VeryLazy",
+	},
+
+	-- Nvim-dap-ui (https://github.com/rcarriga/nvim-dap-ui)
+	--
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = { "mfussenegger/nvim-dap" },
+		event = "VeryLazy",
+	},
 }
