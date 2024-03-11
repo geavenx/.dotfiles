d4d0181a04c9fb9526cd02f3175d9468a815a53a
vitor <vitordonnangeloc@proton.me>
2024-03-11 19:50:58 -0300

    fix: swww kill && swww init to swww kill\n swww init

--- home/.config/scripts/wallpaper.sh
+++ home/.config/scripts/wallpaper.sh
@@ -5,7 +5,8 @@ WALLPAPER_DIR="/mnt/hdd/media/wallpapers"
 RANDOM_WPP=$(ls $WALLPAPER_DIR/* | shuf -n1)
 
 # Starts swww
-swww kill && swww init
+swww kill 
+swww init
 swww img $RANDOM_WPP
 
 # Generate colorscheme
