a52cfbf84d168f3202bf9111bdd0a1398da0d1ec
vitor <vitordonnangeloc@proton.me>
2024-03-18 11:52:56 -0300

    feat: QOL improvements

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -8,10 +8,12 @@ require("mappings")
 -----------------------------------------------------
 -- Core settings
 -----------------------------------------------------
-
 -- Relative line numbers
 vim.opt.nu = true
 vim.opt.relativenumber = true
+
+-- keep signcolumn on by default
+vim.opt.signcolumn = "yes"
 
 -- 4 space indenting & auto indenting
 vim.opt.tabstop = 4
@@ -44,6 +46,23 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 -- Case-insensitive searching UNLESS \C or capital in search
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+
+-- Sets how neovim will display certain whitespace in the editor.
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = "split"
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+
+-- Set highlight on search, but clear on pressing <Esc> in normal mode
+vim.opt.hlsearch = true
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -----------------------------------------------------
 -- Colorscheme
