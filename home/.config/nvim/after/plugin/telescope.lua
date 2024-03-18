f79502326524211371fc240358363d7bf0a6ab5a
vitor <vitordonnangeloc@proton.me>
2024-03-18 11:51:58 -0300

    feat: add telescope builtin mappings

--- home/.config/nvim/after/plugin/telescope.lua
+++ home/.config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,24 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = '<p>roject <f>ind' }) -- pf -> Project Find
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function() --  ps -> Project Search
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end, { desc = "<p>roject <s>earch" })
+require("telescope").setup({
+	extensions = {
+		["ui-select"] = {
+			require("telescope.themes").get_dropdown(),
+		},
+	},
+})
+
+-- Enable telescope extensions, if they are installed
+pcall(require("telescope").load_extension, "fzf")
+pcall(require("telescope").load_extension, "ui-select")
+
+local builtin = require("telescope.builtin")
+local map = function(binding, func, desc)
+	vim.keymap.set("n", binding, func, { desc = desc })
+end
+
+map("<leader>pf", builtin.find_files, "<p>roject <f>ind")
+map("<leader>ps", builtin.live_grep, "<p>roject <s>earch")
+map("<leader><leader>", builtin.buffers, "Existing buffers")
+map("gd", builtin.lsp_definitions, "<g>oto <d>efinition")
+map("gr", builtin.lsp_references, "<g>oto <r>eferences")
+map("gI", builtin.lsp_implementations, "<g>oto <I>mplementations")
+map("D", builtin.lsp_type_definitions, "type <D>efinition")
