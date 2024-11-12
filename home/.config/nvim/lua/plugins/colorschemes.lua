4630ca7fe9daa56b44d6fb6265d528ef62f97935
Vitor Cardoso <vitor.cardoso@sec4you.com.br>
2024-11-12 12:51:37 -0300

    change colorscheme to rose-pine

--- home/.config/nvim/lua/plugins/colorschemes.lua
+++ home/.config/nvim/lua/plugins/colorschemes.lua
@@ -1,21 +1,21 @@
 return {
   -- Change the name of the colorscheme plugin below, and then
-  { -- You can easily change to a different colorscheme.
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
-  },
+  -- { -- You can easily change to a different colorscheme.
+  --   -- change the command in the config to whatever the name of that colorscheme is.
+  --   --
+  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --   'folke/tokyonight.nvim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   init = function()
+  --     -- Load the colorscheme here.
+  --     -- Like many other themes, this one has different styles, and you could load
+  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --     vim.cmd.colorscheme 'tokyonight-night'
+  --
+  --     -- You can configure highlights by doing something like:
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
   -- {
   --   'ellisonleao/gruvbox.nvim',
   --   priority = 1000,
@@ -26,4 +26,12 @@ return {
   --     vim.cmd.hi 'Comment gui=none'
   --   end,
   -- },
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    priority = 1000,
+    init = function()
+      vim.cmd.colorscheme 'rose-pine'
+    end,
+  },
 }
