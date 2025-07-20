feb825af4bd6e62abe4eedefc1820e7d92067f4f
Vitor Cardoso <vitordonnangeloc@proton.me>
2025-07-20 16:56:24 -0300

    add mappings to: source config, find and replace in V, add go tags

--- home/.config/nvim/lua/mappings.lua
+++ home/.config/nvim/lua/mappings.lua
@@ -10,11 +10,20 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 -- }}
 
-vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = '<s>earch and replace all' }) -- Search and replace word's all ocurrencies
-
 -- Keep cursor centered when scrolling {{
 vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'scroll down keeping the cursor on the center' })
 vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'scroll up keeping the cursor on the center' })
 -- }}
 
-vim.keymap.set('n', '<C-t>', '<cmd>:TransparentToggle<CR>', { desc = 'toggle transparency' }) -- Toggle background transparency
+vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = '<s>earch and replace all' }) -- Search and replace word's all ocurrencies
+vim.keymap.set('x', '<leader>s', function() -- Same thing as the above, but only inside a visual line selection
+  local word = vim.fn.expand '<cword>'
+  local escaped_word = vim.fn.escape(word, '/\\.*[]^$')
+  local keymap = vim.api.nvim_replace_termcodes(':s/' .. escaped_word .. '/' .. escaped_word .. '/gI<Left><Left><Left>', true, false, true)
+  vim.api.nvim_feedkeys(keymap, 'n', false)
+end, { desc = '<s>earch and replace word under cursor in visual line mode' })
+
+vim.keymap.set('n', '<leader><leader>', ':so<CR>')
+
+vim.keymap.set('n', '<leader>gj', [[$a`json:""`<Left><Left>]], { desc = 'append json tag to the end of struct' })
+vim.keymap.set('n', '<leader>gx', [[$a`xml:""`<Left><Left>]], { desc = 'append xml tag to the end of struct' })
